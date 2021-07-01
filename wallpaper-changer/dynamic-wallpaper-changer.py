5ae0839aecc288f5c436e7cecb73d47da6323466
Sourav A <souravsunju@gmail.com>
2021-07-01 07:25:39 +0530

    dynamic wallpaper changer working

---
+++ wallpaper-changer/dynamic-wallpaper-changer.py
@@ -0,0 +1,43 @@
+from mac_osx_dynamic import config 
+from time import gmtime, strftime
+from datetime import datetime, time
+import os
+import time as systemTime
+
+
+def is_time_between(begin_time, end_time, check_time=None):
+    # If check time is not given, default to current UTC time
+    check_time = check_time or datetime.utcnow().now()
+    if begin_time < end_time:
+        return check_time >= begin_time and check_time <= end_time
+    else: # crosses midnight
+        return check_time >= begin_time or check_time <= end_time
+
+def is_between(time, time_range):
+    if time_range[1] < time_range[0]:
+        return time >= time_range[0] or time <= time_range[1]
+    return time_range[0] <= time <= time_range[1]
+
+
+
+def main():
+    current_module = "mac_osx_dynamic"
+    wallpapers = config.getWallPaper()
+    current_hour = strftime("%H")
+    current_min = strftime("%M")
+
+    print("current_hour: ", current_hour)
+    print("current_min: ", current_min)
+
+    for timeLapsImage in wallpapers:
+        print(timeLapsImage)
+        start = time(timeLapsImage[0])
+        end = time(timeLapsImage[1])
+        res = is_between(f'{current_hour}:{current_min}',(f'{start}:00', f'{end}:00'))
+        print("CURRENT TIME: ", start, end)
+        if res:
+            wall = "feh --bg-fill ./"+ current_module+"/"+timeLapsImage[2]
+            print(wall)
+            os.system(wall)
+    
+main()
---
+++ wallpaper-changer/mac_osx_dynamic/config.py
@@ -0,0 +1,14 @@
+def getWallPaper():
+    return [
+        # (start_time, end_time, wallpaper_no)
+        (7, 10, "0.jpg") ,
+        (10, 16, "1.jpg"),
+        (16, 17, "2.jpg"),
+        (17, 18, "3.jpg"),
+        (18, 19, "4.jpg"),
+        (19, 2, "5.jpg"),
+        (2, 4, "6.jpg"),
+        (4, 5, "7.jpg"),
+        (5, 7, "8.jpg")
+    ]
+
